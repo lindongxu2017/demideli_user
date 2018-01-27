fda443ce2fc981a2a92b81ff2659278ce8a72656
linzheyu <1017827821@qq.com>
2018-01-27 15:32:45 +0800

    个人名片页面

--- pages/center/card/card.js
+++ pages/center/card/card.js
@@ -3,17 +3,33 @@ const app = getApp()
 const myFn = app.myFn
 const api = app.api
 Page({
+    data: {
+        headImg: '',
+        userInfo: '',
+        animation: ''
+    },
+    onLoad: function (options) {
+        myFn.ajax('post', { session3rd: wx.getStorageSync('session3rd') }, api.user.wxQRcode, res => {
+            this.setData({ headImg: res.data.acode, userInfo: wx.getStorageSync('appInfo')})
+        })
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    headImg: '',
-    userInfo: ''
-  },
-  onLoad: function (options) {
-    myFn.ajax('post', { session3rd: wx.getStorageSync('session3rd') }, api.user.wxQRcode, res => {
-      this.setData({ headImg: res.data.acode, userInfo: wx.getStorageSync('appInfo')})
-    })
-  }
+        var animation = wx.createAnimation({
+            transformOrigin: "50% 50%",
+            duration: 1000,
+            timingFunction: "ease",
+            delay: 0
+        })
+        this.animation = animation;
+        
+        animation.rotate(315).step();
+
+        this.setData({
+            animationData: animation.export()
+        })
+    },
+    callPhone () {
+        wx.makePhoneCall({
+            phoneNumber: '12345678900'
+        })  
+    }
 })
